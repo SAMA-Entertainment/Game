86d4f30b125250ac5ae4234b814571b458cfc03c
Ryuki <thedjplays.gaming@gmail.com>
2022-02-21 19:33:07 +0100

    Added trees to MainMenu (TeamTrees)

--- Assets/Scripts/menus/UIManager.cs
+++ Assets/Scripts/menus/UIManager.cs
@@ -9,140 +9,148 @@ using UnityEngine.Serialization;
 namespace menus
 {
     public class UIManager : MonoBehaviour
-    {public Camera mainCamera;
+    {
+        public Camera mainCamera;
 
-             public GameObject titleGroupObject;
+        public GameObject titleGroupObject;
 
-             public GameObject playGroupObject;
+        public GameObject playGroupObject;
 
-             public GameObject loadingGroupObject;
+        public GameObject loadingGroupObject;
 
-             private readonly Vector3 _cauldronTitlePos = new Vector3(0, 111.6f, 80);
-             private readonly Vector3 _cauldronPlayPos = new Vector3(0, 120, 100);
-             private Quaternion _cauldronPlayRot;
-             private Quaternion _cauldronTitleRot;
-             private CanvasGroup _titleCanvasGroup;
-             private CanvasGroup _playCanvasGroup;
-             private CanvasGroup _loadingCanvasGroup;
+        public GameObject cauldronObject;
 
-             private AsyncOperation _sceneLoading;
+        private Vector3 _cauldronTitlePos = new Vector3(0, 111.6f, 80);
+        private readonly Vector3 _cauldronPlayPos = new Vector3(0, 120, 100);
+        private Vector3 _targetDirection;
+        private Vector3 _nextDirection;
+        private Quaternion _cauldronPlayRot;
+        private Quaternion _cauldronTitleRot;
+        private CanvasGroup _titleCanvasGroup;
+        private CanvasGroup _playCanvasGroup;
+        private CanvasGroup _loadingCanvasGroup;
 
-             private bool _playMenu;
-             private float _camProgression = 0;
-             private float _titleAlphaProgression = 0;
-             private float _playAlphaProgression = 0;
-             private float _loadingScreenAlphaProgression = 0;
+        private AsyncOperation _sceneLoading;
 
-             public void PlayGame()
-             {
-                 // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
-                 _playMenu = true;
-             }
+        private bool _playMenu;
+        private float _camProgression = 0;
+        private float _titleAlphaProgression = 0;
+        private float _playAlphaProgression = 0;
+        private float _loadingScreenAlphaProgression = 0;
 
-             public void JoinGame()
-             {
-                 _sceneLoading = SceneManager.LoadSceneAsync("Game");
-                 _sceneLoading.allowSceneActivation = false;
-             }
+        public void PlayGame()
+        {
+            // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+            _playMenu = true;
+        }
 
-             public void ReturnBack()
-             {
-                 _playMenu = false;
-             }
+        public void JoinGame()
+        {
+            _sceneLoading = SceneManager.LoadSceneAsync("Game");
+            _sceneLoading.allowSceneActivation = false;
+        }
 
-             public void Start()
-             {
-                 _cauldronPlayRot = Quaternion.Euler(90, 0, 0);
-                 _cauldronTitleRot = Quaternion.Euler(15, 0, 0);
-                 _playMenu = false;
-                 _camProgression = 0;
-                 _titleAlphaProgression = 1;
-                 _playAlphaProgression = 0;
-                 _loadingScreenAlphaProgression = 0;
-                 _titleCanvasGroup = titleGroupObject.GetComponent<CanvasGroup>();
-                 _playCanvasGroup = playGroupObject.GetComponent<CanvasGroup>();
-                 _loadingCanvasGroup = loadingGroupObject.GetComponent<CanvasGroup>();
-                 mainCamera.transform.position = _cauldronTitlePos;
-             }
+        public void ReturnBack()
+        {
+            _playMenu = false;
+        }
 
-             public void Update()
-             {
-                 if (_playMenu)
-                 {
-                     _titleCanvasGroup.interactable = false;
-                     playGroupObject.SetActive(true);
-                     if (_camProgression < 1)
-                         _camProgression += 0.01f;
-                     else
-                     {
-                         titleGroupObject.SetActive(false);
-                         _playCanvasGroup.interactable = true;
-                     }
+        public void Start()
+        {
+            _cauldronPlayRot = Quaternion.Euler(90, 0, 0);
+            _cauldronTitleRot = Quaternion.Euler(15, 0, 0);
+            _playMenu = false;
+            _camProgression = 0;
+            _titleAlphaProgression = 1;
+            _playAlphaProgression = 0;
+            _loadingScreenAlphaProgression = 0;
+            _titleCanvasGroup = titleGroupObject.GetComponent<CanvasGroup>();
+            _playCanvasGroup = playGroupObject.GetComponent<CanvasGroup>();
+            _loadingCanvasGroup = loadingGroupObject.GetComponent<CanvasGroup>();
+            mainCamera.transform.position = _cauldronTitlePos;
+        }
 
-                     if (_playAlphaProgression < 1)
-                         _playAlphaProgression += 0.01f;
+        public void FixedUpdate()
+        {
+            if (_playMenu)
+            {
+                _titleCanvasGroup.interactable = false;
+                playGroupObject.SetActive(true);
+                if (_camProgression <= 1)
+                    _camProgression += 0.01f;
+                else
+                {
+                    titleGroupObject.SetActive(false);
+                    _playCanvasGroup.interactable = true;
+                }
 
-                     if (_titleAlphaProgression < 1)
-                         _titleAlphaProgression += 0.05f;
-                 }
-                 else
-                 {
-                     _playCanvasGroup.interactable = false;
-                     if (_camProgression > 0)
-                         _camProgression -= 0.01f;
-                     else
-                     {
-                         playGroupObject.SetActive(false);
-                         titleGroupObject.SetActive(true);
-                         _titleCanvasGroup.interactable = true;
-                         if (_titleAlphaProgression > 0)
-                             _titleAlphaProgression -= 0.05f;
-                     }
+                if (_playAlphaProgression < 1)
+                    _playAlphaProgression += 0.01f;
 
-                     if (_playAlphaProgression > 0)
-                         _playAlphaProgression -= 0.01f;
-                 }
+                if (_titleAlphaProgression < 1)
+                    _titleAlphaProgression += 0.05f;
+            }
+            else
+            {
+                _playCanvasGroup.interactable = false;
+                if (_camProgression > 0)
+                    _camProgression -= 0.01f;
+                else
+                {
+                    playGroupObject.SetActive(false);
+                    titleGroupObject.SetActive(true);
+                    _titleCanvasGroup.interactable = true;
+                    if (_titleAlphaProgression > 0)
+                        _titleAlphaProgression -= 0.05f;
+                }
 
-                 if (_sceneLoading != null)
-                 {
-                     loadingGroupObject.SetActive(true);
-                     if (_loadingScreenAlphaProgression < 1)
-                         _loadingScreenAlphaProgression += 0.1f;
+                if (_playAlphaProgression > 0)
+                    _playAlphaProgression -= 0.01f;
+            }
 
-                     loadingGroupObject.GetComponentInChildren<TextMeshProUGUI>().text =
-                         $"Chargement...\n{Math.Ceiling(_sceneLoading.progress / 0.9):P}";
+            if (_sceneLoading != null)
+            {
+                loadingGroupObject.SetActive(true);
+                if (_loadingScreenAlphaProgression < 1)
+                    _loadingScreenAlphaProgression += 0.1f;
 
-                 }
-                 else
-                 {
-                     if (_loadingScreenAlphaProgression > 0)
-                         _loadingScreenAlphaProgression -= 0.1f;
-                     else
-                         loadingGroupObject.SetActive(false);
-                 }
-             }
+                loadingGroupObject.GetComponentInChildren<TextMeshProUGUI>().text =
+                    $"Chargement...\n{Math.Ceiling(_sceneLoading.progress / 0.9):P}";
+            }
+            else
+            {
+                if (_loadingScreenAlphaProgression > 0)
+                    _loadingScreenAlphaProgression -= 0.1f;
+                else
+                    loadingGroupObject.SetActive(false);
+            }
 
-             public void LateUpdate()
-             {
-                 _titleCanvasGroup.alpha = EaseInOutSine(1 - _titleAlphaProgression);
-                 _playCanvasGroup.alpha = EaseInOutSine(_playAlphaProgression);
-                 _loadingCanvasGroup.alpha = _loadingScreenAlphaProgression;
-                 mainCamera.transform.position =
-                     Vector3.Slerp(_cauldronTitlePos, _cauldronPlayPos, EaseInOutSine(_camProgression));
-                 mainCamera.transform.rotation =
-                     Quaternion.Lerp(_cauldronTitleRot, _cauldronPlayRot, EaseInOutSine(_camProgression));
-                 if (_sceneLoading != null && _sceneLoading.progress >= 0.9f)
-                 {
-                     _sceneLoading.allowSceneActivation = true;
-                 }
-             }
+            _cauldronTitleRot =
+                Quaternion.LookRotation(cauldronObject.transform.position - mainCamera.transform.position);
+            _titleCanvasGroup.alpha = EaseInOutSine(1 - _titleAlphaProgression);
+            _playCanvasGroup.alpha = EaseInOutSine(_playAlphaProgression);
+            _loadingCanvasGroup.alpha = _loadingScreenAlphaProgression;
+        }
 
-             public void QuitGame()
-             {
-                 Debug.Log("=> Quit");
-                 Application.Quit();
-             }
+        public void LateUpdate()
+        {
+            mainCamera.transform.position =
+                Vector3.Slerp(_cauldronTitlePos, _cauldronPlayPos, EaseInOutSine(_camProgression));
+            mainCamera.transform.rotation =
+                Quaternion.Lerp(_cauldronTitleRot, _cauldronPlayRot, EaseInOutSine(_camProgression));
 
-             private float EaseInOutSine(float x) => (float) (-(Math.Cos(Math.PI * x) - 1) / 2);
+            if (_sceneLoading != null && _sceneLoading.progress >= 0.9f)
+            {
+                _sceneLoading.allowSceneActivation = true;
+            }
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("=> Quit");
+            Application.Quit();
+        }
+
+        private float EaseInOutSine(float x) => (float) (-(Math.Cos(Math.PI * x) - 1) / 2);
     }
 }
