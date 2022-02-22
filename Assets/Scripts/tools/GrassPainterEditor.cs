7d063f49466dab00b64f5765fed667d0d56f3e2c
Ryuki <thedjplays.gaming@gmail.com>
2022-02-22 20:30:11 +0100

    Fix Game not build due to GrassPainterEditor

--- Assets/Scripts/tools/GrassPainterEditor.cs
+++ Assets/Scripts/tools/GrassPainterEditor.cs
@@ -1,3 +1,4 @@
+#if (UNITY_EDITOR)
 using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
@@ -80,3 +81,8 @@ public class GrassPainterEditor : Editor
     }
 
 }
+#else
+public class GrassPainterEditor
+{
+}
+#endif
