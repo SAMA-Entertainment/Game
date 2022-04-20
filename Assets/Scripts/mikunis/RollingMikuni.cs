9cac59146569a22f089d3fa159b4b5a90a197994
Sofiane <sofiane.meftah@epita.fr>
2022-04-20 13:07:19 +0200

    Added potato and corn Mikunis and Updated All Mikunis (behaviour change, new eyes, new size)

--- Assets/Scripts/mikunis/RollingMikuni.cs
+++ Assets/Scripts/mikunis/RollingMikuni.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 namespace mikunis
 {
-    public class RollingMikuniController : Mikuni
+    public class RollingMikuni : Mikuni
     {
 
         public Transform rotatingBody;
+        public Vector3 rotateDir = Vector3.right;
         public float rotSpeedX = 250f;
 
         protected override void FixedUpdate()
@@ -13,7 +14,7 @@ namespace mikunis
             base.FixedUpdate();
             
             float speed = agent.velocity.magnitude/agent.speed;
-            rotatingBody.Rotate(Vector3.right, rotSpeedX * speed * Time.deltaTime);
+            rotatingBody.Rotate(rotateDir, rotSpeedX * speed * Time.deltaTime);
         }
     }
 }
